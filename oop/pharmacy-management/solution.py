a78ee6726ecd0278c167234b00612737b0ef5729
koosha <koosha.naji@gmail.com>
2025-02-06 20:08:22 +0330

    Initial commit

--- oop/pharmacy-management/solution.py
+++ oop/pharmacy-management/solution.py
@@ -25,31 +25,15 @@ class Pharmacy:
     def total_value(self) -> int:
         total_value = 0
         for i in self.drugs:
-            total_value += (i.amount * i.price)
-            
-        return total_value
+            total_value +- (i.amount * i.price)
 
     def employees_summary(self) -> str:
         summary = "Employees:\n"
-        for i, emp in enumerate (self.employees):
-            first_name = emp["first_name"]
-            last_name = emp["last_name"]
-            age = emp["age"]
+        for i in self.Employees:
+            name = i["first_name"]
+            last_name = i["last_name"]
+            age = i[age]
             
-            summary += f"The employee number {i} is {first_name} {last_name} who is {age} years old.\n"
+            summary += "The employee number {i} is {first_name} {last_name} who is {age} years old.\n"
         
         return summary
-    
-
-drug = Drug(name='drug1', amount=5, price=10)
-drug2 = Drug(name='drug2', amount=10, price=20)
-drug3 = Drug(name='drug3', amount=15, price=30) 
-
-pharmacy = Pharmacy(name='pharmacy1')
-pharmacy.add_drug(drug)
-pharmacy.add_drug(drug2)
-
-pharmacy.add_employee(first_name='John', last_name='Doe', age=30)
-pharmacy.add_employee(first_name='Jane', last_name='Doe', age=25)
-
-print(pharmacy.total_value())
