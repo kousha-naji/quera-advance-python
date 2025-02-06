58eaa719040eb42c122f585967eae53e3dd5108d
koosha <koosha.naji@gmail.com>
2025-02-06 22:05:35 +0330

    vehicle build obj

---
+++ oop/vehicle/test/__init__.py
@@ -0,0 +1,4 @@
+from airplane import B707
+
+test1 = B707(airline='Iran Air', max_speed=2000, steering_wheel=7, number_of_wheels=7, number_of_seats=200,
+             captain='Koosha', number_of_crew=10, price='2_000_000', name='Ziba')
