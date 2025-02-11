9b5710b2079c49a17a8c1b36a98a7fc6d87d50dc
koosha <koosha.naji@gmail.com>
2025-02-11 16:46:49 +0330

    add work_place in the oop project

---
+++ oop/project/work-place-management/test/company.py
@@ -0,0 +1,18 @@
+# YOUR IMPORTS GO HERE
+from work_place import WorkPlace, Consts
+
+
+class Company(WorkPlace):
+    def __init__(self, name):
+        super().__init__(name)
+        self.expertise = "company"
+
+
+
+    def calc_capacity(self):
+        self.capacity = self.level
+
+    def calc_costs(self):
+        self.costs = Consts.BASE_PLACE_COST * self.level
+        return self.costs
+
